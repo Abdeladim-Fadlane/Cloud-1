e0b0ef493acfa21c36a1eb47d597e451797d5a95
Abdeladim-Fadlane <abdofadlane128@gmail.com>
2025-08-03 16:49:32 +0100

    add cleanup task

--- .sh
+++
@@ -1,8 +0,0 @@
-docker stop $(docker ps -q)
-
-docker rm -f $(docker ps -aq)
-docker rmi -f $(docker images -q)
-docker volume rm $(docker volume ls -q)
-
-echo -e "Docker system cleaned up."
-docker system df 
--- roles/inception/srcs/requirements/nginx/entrypoint.sh
+++ roles/inception/srcs/requirements/nginx/entrypoint.sh
@@ -10,8 +10,8 @@ mkdir -p "$NGINX_SSL_PATH"
 # Check if SSL certificate already exists
 if [ ! -f "$CERT_PATH" ]; then
     echo "No SSL certificate found. Generating a new one..."
-    certbot certonly --standalone --agree-tos --email your-email@domain.com \
-    -d garb-anzo.tech  --expand --non-interactive
+    certbot certonly --standalone --agree-tos --email abdofadlane128@gmail.com \
+    -d garb-anzo.tech -d afadlane.garb-anzo.tech -d www.garb-anzo.tech --expand --non-interactive
 else
     echo "SSL certificate already exists. Skipping Certbot."
 fi
